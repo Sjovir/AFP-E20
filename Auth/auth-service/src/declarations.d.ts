05c671a64b4b411338ee547781397be1ab7c1514
Erik Bjørnholt <erik@hagebjerg.dk>
2020-09-04 15:00:30 +0200

    Established connection to docker DB when using docker-compose

--- Auth/auth-service/src/declarations.d.ts
+++ Auth/auth-service/src/declarations.d.ts
@@ -4,5 +4,10 @@ declare namespace NodeJS
     {
         NODE_ENV: "development" | "production";
         JWT_SECRET: string;
+        DB_ROOT_PASSWORD: string;
+        DB_USER: string;
+        DB_PASSWORD: string;
+        DB_DATABASE: string;
+        DB_HOST: string;
     }
 }
