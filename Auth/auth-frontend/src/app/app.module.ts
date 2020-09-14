a775709218ff3a68a7afc6a78645562063826287
Dennis Petersen <petersen.dnns@gmail.com>
2020-09-14 14:16:39 +0200

    layout and button implemented

--- Auth/auth-frontend/src/app/app.module.ts
+++ Auth/auth-frontend/src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Auth/auth-frontend/src/app/register/register.component.ts
+++ Auth/auth-frontend/src/app/register/register.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  public registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuiler: FormBuilder) {}
 
   ngOnInit(): void {
+    this.registerForm = this.formBuiler.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      cpr: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+    });
   }
 
+  onSubmit(): void {
+    // [disabled]="!registerForm.valid"
+    console.log('works?');
+  }
+
+  get emailControl(): AbstractControl {
+    return this.registerForm.get('email') as AbstractControl;
+  }
+
+  get passwordControl(): AbstractControl {
+    return this.registerForm.get('password') as AbstractControl;
+  }
+
+  get cprControl(): AbstractControl {
+    return this.registerForm.get('cpr') as AbstractControl;
+  }
+
+  get firstNameControl(): AbstractControl {
+    return this.registerForm.get('firstName') as AbstractControl;
+  }
+
+  get lastNameControl(): AbstractControl {
+    return this.registerForm.get('lastName') as AbstractControl;
+  }
 }
