c88a1be7dd8e0b14aaf9045bd51ea0efa1132bf6
Erik Bjørnholt <erik@hagebjerg.dk>
2020-09-22 09:49:37 +0200

    Add Bosted Frontend

---
+++ Bosted/bosted-frontend/src/app/citizen/journal/journal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'citizen-journal',
+  templateUrl: './journal.component.html',
+  styleUrls: ['./journal.component.scss']
+})
+export class JournalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
