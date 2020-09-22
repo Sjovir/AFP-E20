23b51c7654690866f76fc53f10d3c225c18319b0
Erik Bjørnholt <erik@hagebjerg.dk>
2020-09-22 13:08:58 +0200

    Polished navigation top bar

--- Bosted/bosted-frontend/src/app/app.module.ts
+++ Bosted/bosted-frontend/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { MenuComponent } from './nav/menu/menu.component';
 import { TopBarComponent } from './nav/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     MenuComponent,
     TopBarComponent
   ],
