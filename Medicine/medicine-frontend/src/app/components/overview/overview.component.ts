088d36b86893fa039d847390f84cf8c226f3bec9
Erik Bjørnholt <erik@hagebjerg.dk>
2020-10-19 15:47:37 +0200

    Setup medicine-overview page

--- Medicine/medicine-frontend/src/app/components/overview/overview.component.ts
+++ Medicine/medicine-frontend/src/app/components/overview/overview.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Ordination } from 'src/app/models/ordination.model';
+import { MedicineService } from 'src/app/services/medicine.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.scss']
+  styleUrls: ['./overview.component.scss'],
 })
 export class OverviewComponent implements OnInit {
+  public ordinations: Ordination[];
 
-  constructor() { }
+  constructor(
+    private jwtHelper: JwtHelperService,
+    private medicineService: MedicineService,
+    private router: Router
+  ) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const route = event.urlAfterRedirects;
 
-  ngOnInit(): void {
+        console.log(route);
+
+        this.medicineService.getOrdinations(route).subscribe((ordinations) => {
+          this.ordinations = ordinations;
+        });
+      });
   }
 
+  ngOnInit(): void {}
+
+  public editCitizen() {
+    console.log('Edit Citizen modal');
+  }
+
+  public createOrdination() {
+    console.log('Create Ordination Page');
+  }
+
+  public editOrdination(ordinationId: string) {
+    console.log('Edit Ordination Page');
+  }
 }
--- Medicine/medicine-frontend/src/app/services/medicine.service.ts
+++ Medicine/medicine-frontend/src/app/services/medicine.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Ordination } from '../models/ordination.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,14 @@ import { Injectable } from '@angular/core';
 export class MedicineService {
 
   constructor() { }
+
+  public  getOrdinations(userId: string): Observable<Ordination[]> {
+    const ordinations: Ordination[] = [
+      { id: 'o1', drug: { id: 'd1', name: 'Panodil', code: 'E20' }, drugAmount: 5, drugUnit: 'mg', startDate: new Date(2020, 10, 18, 8), endDate: null },
+      { id: 'o2', drug: { id: 'd2', name: 'Metadon', code: 'E45' }, drugAmount: 2, drugUnit: 'ml', startDate: new Date(2020, 10, 10, 8), endDate: null },
+      { id: 'o3', drug: { id: 'd3', name: 'Nikotin Tyggegummi', code: 'A5' }, drugAmount: 1, drugUnit: 'stk', startDate: new Date(2019, 11, 11, 8), endDate: null },
+    ];
+
+    return of(ordinations);
+  }
 }
