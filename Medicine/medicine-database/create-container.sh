2590362ebee87e0dafec4de6edc272d237b554ce
Dennis Petersen <petersen.dnns@gmail.com>
2020-10-23 11:42:42 +0200

    init

---
+++ Medicine/medicine-database/create-container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+IMAGE_NAME=medicine-mdb-image
+CONTAINER_NAME=medicine-mdb
+CONTAINER_EXISTS=$(docker ps -aq -f name="${CONTAINER_NAME}")
+
+echo $CONTAINER_EXISTS
+
+if [ "$CONTAINER_EXISTS" ]; then
+    echo 'Already exists!'
+else
+    docker build -t $IMAGE_NAME .
+    docker run --name $CONTAINER_NAME -p 3308:3306 -e MYSQL_ROOT_PASSWORD=toor -d $IMAGE_NAME
+fi
