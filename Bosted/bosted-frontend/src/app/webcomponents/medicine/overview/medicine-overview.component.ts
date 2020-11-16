4d27fc634621b317af798c305f16d27c7d662810
Erik Bjørnholt <erik@hagebjerg.dk>
2020-11-16 13:29:52 +0100

    Add build mode to BostedFrontend

--- Bosted/bosted-frontend/src/app/webcomponents/medicine/overview/medicine-overview.component.ts
+++ Bosted/bosted-frontend/src/app/webcomponents/medicine/overview/medicine-overview.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'medicine-overview',
   templateUrl: './medicine-overview.component.html',
 })
 export class MedicineOverviewComponent implements OnInit {
-  link: string = 'http://localhost:8200/main.js';
+  link: string = `http://${environment.host}:8200/main.js`;
 
   @Input() dataToChild: string;
   @Output() dataFromChild = new EventEmitter<string>();
--- Bosted/bosted-frontend/src/environments/environment.prod.ts
+++ Bosted/bosted-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  host: '46.101.172.215'
 };
