99f061a56e3fa05027d4ede93c3eb1ab107940a1
Erik Bjørnholt <erik@hagebjerg.dk>
2021-01-14 12:07:51 +0100

    fixed first integration test

--- test/test_integration.sh
+++ test/test_integration.sh
@@ -7,9 +7,17 @@
 ##########################
 #####      AUTH      #####
 ##########################
-url=http://localhost:7000/api/register
-echo $url
+auth_url=http://46.101.172.215:7000/api
+echo $auth_url
 
+login_payload=integration/login.json
 register_payload=integration/register.json
-echo $register_payload
-curl -X POST -H "Content-Type: application/json" -d @$register_payload $url
+
+echo "\n\n** Register **"
+curl -X POST -H "Content-Type: application/json" -d @$register_payload ${auth_url}/register
+
+echo "\n\n** Login **"
+curl -X POST -H "Content-Type: application/json" -d @$login_payload ${auth_url}/login
+echo "\n\n** Get User **"
+curl -X GET ${auth_url}/users/username/kk
+
